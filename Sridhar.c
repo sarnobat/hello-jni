a4bc21d0acb2038fe46c3c7b46fae30b3b68e385
Sridhar Sarnobat <ss401533@gmail.com>
2013-08-22 13:58:14 -0700

    Originator's sample didn't work on Mac. Removed Makefile and replaced with shell script

--- Sridhar.c
+++ Sridhar.c
@@ -3,6 +3,6 @@
 #include <stdio.h>
 
 JNIEXPORT void JNICALL Java_Sridhar_print (JNIEnv *env, jobject obj) {
-  printf("Helloworld.c :: Java_Sridhar_print() :: begin\n");
+  printf(".c :: Java_Sridhar_print() :: begin\n");
 }
 
