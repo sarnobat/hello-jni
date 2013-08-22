2caefa84acebf3c6744616c79726c87be01f202d
Sridhar Sarnobat <ss401533@gmail.com>
2013-08-22 13:58:14 -0700

    Originator's sample didn't work on Mac. Removed Makefile and replaced with shell script

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,13 +3,15 @@ public class HelloWorld
     public native void print();
 
     static {
-	System.out.println("Loading shared lib");
-        System.loadLibrary("helloWoRLd"); // not case sensitive?
+	System.out.println("Helloworld.java::static - Loading shared library 'helloworld'");
+        System.loadLibrary("helloWoRLd"); // not case sensitive
     }
 
     public static void main(String[] args) {
-	System.out.println("Hello World!");
+	System.out.println("HelloWorld.java :: main() :: Begin");
 	HelloWorld hw = new HelloWorld();
+	System.out.println("Helloworld.java :: main() :: About to call native print() method");
+	System.out.println();
 	hw.print();
     }
 }
