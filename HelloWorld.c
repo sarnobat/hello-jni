cb775f79a34accee515993859f377f8a9b7ed4f3
Sridhar Sarnobat <ss401533@gmail.com>
2013-08-22 13:58:14 -0700

    Originator's sample didn't work on Mac. Removed Makefile and replaced with shell script

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,6 +3,6 @@
 #include <stdio.h>
 
 JNIEXPORT void JNICALL Java_HelloWorld_print (JNIEnv *env, jobject obj) {
-  printf("Helloworld.c::Java_HelloWorld_print() - begin\n");
+  printf("Helloworld.c :: Java_HelloWorld_print() :: begin\n");
 }
 
