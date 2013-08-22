c213b742bb503b7fad44f2ee2e14fd4dd7e3df65
Sridhar Sarnobat <ss401533@gmail.com>
2013-08-22 13:58:14 -0700

    Originator's sample didn't work on Mac. Removed Makefile and replaced with shell script

--- Sridhar.c
+++ Sridhar.c
@@ -4,7 +4,10 @@
 #include <sys/time.h>
 
 JNIEXPORT void JNICALL Java_Sridhar_printx (JNIEnv *env, jobject obj) {
+	// Simple print statement
 	printf(".c :: Java_Sridhar_print() :: begin\n");
+	
+	// Call an existing library (from time.h)
 	struct timeval te; 
 	gettimeofday(&te, NULL); // get current time
 	long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // caculate milliseconds
